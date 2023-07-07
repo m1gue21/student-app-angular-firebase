c4f1ef1cae42f2c770e527f00c7cb4112a4b127d
m1gue21 <miguefreeagent@gmail.com>
2023-07-07 15:13:52 -0500

    firebase setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'students-8eec0',
+    appId: '1:88546430916:web:fba0ce1b45085931a19e91',
+    storageBucket: 'students-8eec0.appspot.com',
+    apiKey: 'AIzaSyCie9dFVO9s-kR5DvhJy-MyTLgrnRRpZx4',
+    authDomain: 'students-8eec0.firebaseapp.com',
+    messagingSenderId: '88546430916',
+  },
+};
